68e236e0d43fd1fd53573d98d51ee4277c7d24df
gauravapatil <gp952741@gmail.com>
2024-07-30 21:56:40 +0530

    Changes made

---
+++ script.js
@@ -0,0 +1,14 @@
+let box=document.querySelector(".container");
+let text=document.querySelector(".text");
+
+// Add a margin of 20 pixels to the element using the style.margin property.(10 Marks)
+// Apply a padding of 10 pixels using the style.padding property.(10 Marks)
+// Change the font size to 18 pixels using the style.fontSize property.(10 Marks)
+// Set the font weight to bold using the style.fontWeight property. (10 Marks)
+// Change the height of the element to 200 pixels using the style.height property. (10 Marks)
+// Modify the width of the element to 300 pixels using the style.width property.(10 Marks)
+box.style.backgroundColor="yellow";
+box.style.margin="20px";
+text.style.fontWeight="bold";
+text.style.fontSize="18px";
+box.style.height="300px";
